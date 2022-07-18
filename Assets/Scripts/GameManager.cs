5ecd82510ff5836b1129a0c889c8e914dc016b1d
bambi0256 <bambi0256@gmail.com>
2022-07-18 19:39:49 +0900

    Set Dictionary Valuations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    //호감도 레벨 저장
+    //호감도 수치 저장
     public float fox_level;
-    public float dok_level;
+    public float dob_level;
     public float woo_level;
     
+    //스크립트 Save
+    public Dictionary<int, CScript> Save;
+
+    //
+
     //이전 씬 저장
     public string PrevScene;
 
-    
     //파괴불가
     private static GameManager _instance;
     public static GameManager Instance
